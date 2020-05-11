8ea01eeb279c997e7a3287906ef79fb2a6eecef4
Toby Essex <tobysx@googlemail.com>
2020-05-11 22:12:36 +0100

    Updated player's board game object to calculate visible score and face-up cards.

--- gobjects/PlayerBoard.js
+++ gobjects/PlayerBoard.js
@@ -26,8 +26,59 @@ class PlayerBoard {
         this.cards[column].push(card);
     }
 
+    /**
+     * @public
+     *
+     * Gets the score of the player's board from all the face up cards.
+     *
+     * @returns {number} - Returns the player's score
+     */
+    visibleScore() {
+        let score = 0;
+
+        for (let card of this.flattenCards())
+            if (card.faceUp)
+                score += card.value;
+
+        return score;
+    }
+
+    /**
+     * @public
+     *
+     * Returns the number of face-up cards in the player's board.
+     *
+     * @returns {number} - Returns the number of face-up cards.
+     */
+    noOfFaceUpCards() {
+        let count = 0;
+
+        for (let card of this.flattenCards())
+            if (card.faceUp)
+                count += 1;
+
+        return count;
+    }
+
+    /**
+     * @private
+     *
+     * @returns {Card[]} - Returns a single dimension array of the cards.
+     */
+    flattenCards() {
+        return this.cards.flat();
+    }
+
     get cards() {
         return this.cards;
+    }
+
+    toJSON() {
+        return {
+            ...this,
+            visibleScore: this.visibleScore(),
+            noOfFaceUpCards: this.noOfFaceUpCards(),
+        }
     }
 }
 
