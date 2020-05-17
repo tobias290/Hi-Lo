76fe6fdf609a144e74373f208521e3f9cede00d0
Toby Essex <tobysx@googlemail.com>
2020-05-17 18:56:58 +0100

    Added missing build files.

---
+++ build/precache-manifest.766724e92fdfeb1d0f584c2599cf965f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dcac0dba0baa03aaaf404196d0ebeb3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f2dc6569f8b1afb2d123",
+    "url": "./static/css/main.129e4ff7.chunk.css"
+  },
+  {
+    "revision": "97f062b39384e89073ba",
+    "url": "./static/js/2.dca5ffb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dca5ffb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2dc6569f8b1afb2d123",
+    "url": "./static/js/main.1064b4ec.chunk.js"
+  },
+  {
+    "revision": "62fe4d1c496e382b57d5",
+    "url": "./static/js/runtime-main.46096224.js"
+  }
+]);
